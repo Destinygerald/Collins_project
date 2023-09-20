fc91e05ef465acc66885c03d0662fcabfd98b61c
Destinygerald <118162858+Destinygerald@users.noreply.github.com>
2023-09-20 14:24:14 +0100

    Add files via upload

---
+++ Agency/script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const registrationForm = document.getElementById('registration-form');
+
+    registrationForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        const agencyName = document.getElementById('agency-name').value;
+        const contactPerson = document.getElementById('contact-person').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match. Please try again.");
+            return;
+        }
+
+        // Send registration data to the server or perform any necessary validation.
+        // You can make an AJAX request here to send data to your backend.
+
+        // For demonstration purposes, we'll log the registration data to the console.
+        console.log('Registration submitted:');
+        console.log('Agency Name:', agencyName);
+        console.log('Contact Person:', contactPerson);
+        console.log('Email:', email);
+        console.log('Password:', password);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Send login data to the server or perform any necessary validation.
+        // You can make an AJAX request here to send data to your backend.
+
+        // For demonstration purposes, we'll log the login data to the console.
+        console.log('Login submitted:');
+        console.log('Email:', email);
+        console.log('Password:', password);
+
+        // Redirect to the dashboard or another page upon successful login.
+        // Replace the following line with your actual redirection logic.
+        // window.location.href = '/dashboard.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const logoutButton = document.getElementById('logout-button');
+
+    logoutButton.addEventListener('click', function () {
+        // Implement logout logic here (e.g., clearing session, redirecting to login page).
+        // You can add your logout functionality or AJAX request to the server.
+
+        // For demonstration purposes, we'll redirect to the login page.
+        window.location.href = 'login.html';
+    });
+});
